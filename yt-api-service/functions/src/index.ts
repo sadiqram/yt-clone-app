c76b59491fa17bb416f014ba817c559b223028eb
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-03-25 21:52:56 -0400

    feat: integrate video upload flow across frontend and backend

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,10 +2,18 @@ import * as functions from "firebase-functions/v1";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+// chore: create some kind of
+// config to store these values after course completion
+const rawVidBucket = "sram-yt-raw-vids";
+
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -24,4 +32,32 @@ export const createUser = functions.auth.user().onCreate((user) => {
   return;
 });
 
+// generate a signed URL for the raw video
+export const generateUploadUrl = onCall(
+  {maxInstances: 1},
+  async (request) => {
+    // Check if user is authenticated
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "User must be authenticated to generate an upload URL"
+      );
+    }
 
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVidBucket);
+
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
+  }
+);
